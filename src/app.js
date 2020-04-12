329612d9cc73c280cb5e9be55a4a11280540f725
Nikhil Kaushik <nikhilka585@gmail.com>
2020-04-12 17:27:14 +0530

    Saving response object in redis store

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ const path = require('path');
 const config = require('./config');
 const { v1 } = require('./routes');
 const fastify = require('fastify')(config.fastify);
-const oauthPlugin = require('fastify-oauth2')
-const {CovidSheetData} = require('./spreadsheet/covid');
+const oauthPlugin = require('fastify-oauth2');
+const { CovidSheetData } = require('./spreadsheet/covid');
 
 // Export fastify for testing purpose
 module.exports = fastify;
@@ -25,7 +25,9 @@ module.exports = fastify;
         // Plugins
         fastify.register(require('fastify-boom'));
         //fastify.register(require('fastify-swagger'), config.documentation);
-        fastify.register(require('fastify-static'), { root: path.join(__dirname, '/public') });
+        fastify.register(require('fastify-static'), {
+            root: path.join(__dirname, '/public'),
+        });
         const dev = process.env.NODE_ENV !== 'production';
         fastify.register(require('fastify-nextjs'), { dev }).after(() => {
             fastify.next('/');
@@ -37,30 +39,36 @@ module.exports = fastify;
             name: 'googleOAuth2',
             scope: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
             credentials: {
-              client: {
-                id: process.env.CLIENT_ID,
-                secret: process.env.CLIENT_SECRET
-              },
-              auth: oauthPlugin.GOOGLE_CONFIGURATION
+                client: {
+                    id: process.env.CLIENT_ID,
+                    secret: process.env.CLIENT_SECRET,
+                },
+                auth: oauthPlugin.GOOGLE_CONFIGURATION,
             },
             // register a fastify url to start the redirect flow
             startRedirectPath: '/login/google',
             // facebook redirect here after the user login
-            callbackUri: 'http://localhost:3000/login/google/callback'
-          })
+            callbackUri: 'http://localhost:3000/login/google/callback',
+        });
         // Server
 
-        fastify.get('/login/google/callback', async function (request, reply) {
-            const token = await this.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request)
+        fastify.get('/login/google/callback', async function(request, reply) {
+            const token = await this.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(
+                request
+            );
             // if later you need to refresh the token you can use
             // const newToken = await this.getNewAccessTokenUsingRefreshToken(token.refresh_token)
             CovidSheetData(token);
             reply.redirect('/');
-          });
+        });
 
         await fastify.listen(config.port, '0.0.0.0');
         //fastify.swagger();
-        fastify.log.info('%s listening in %s environment', config.name, process.env.NODE_ENV);
+        fastify.log.info(
+            '%s listening in %s environment',
+            config.name,
+            process.env.NODE_ENV
+        );
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
--- src/spreadsheet/covid.js
+++ src/spreadsheet/covid.js
@@ -1,91 +1,103 @@
-const { google } = require("googleapis");
-const fs = require("fs");
+const { google } = require('googleapis');
 
-const COVID_JSON_PATH = "covidData.json";
+const client = require('redis').createClient(process.env.REDIS_URL);
 
-const CovidSheetData = token => {
-  const sheets = google.sheets({ version: "v4", token });
+const CovidSheetData = (token) => {
+    const sheets = google.sheets({ version: 'v4', token });
 
-  sheets.spreadsheets.values.get(
-    {
-      auth: "AIzaSyCukHocZ6K1biwSYFPNQmOs_-OL7U8fnkk",
-      spreadsheetId: "1uKqaxMd5gkghBKMmp_fbnLRFLdic_Kv-i_802f77Ih0",
-      range: "Main!A1:G35"
-    },
-    (err, res) => {
-      if (err) return console.log("The API returned an error: " + err);
-      
-      const rows = res.data.values.filter(data => data.length !== 0);
-      if (rows.length) {
-        let headers = rows[0]
-          .concat(rows[1])
-          .filter(
-            i => i.length !== 0 && i !== "MOHFW Data" && i !== "State/Region"
-          );
-        let totalOfCovid = rows[rows.length - 1].filter(i => i.length !== 0);
-        let dataNumbers = rows.slice(2).slice(0, -1);
-        let states = dataNumbers.map(
-          item => item.filter(i => isNaN(parseInt(i)))[0]
-        );
+    sheets.spreadsheets.values.get(
+        {
+            auth: 'AIzaSyCukHocZ6K1biwSYFPNQmOs_-OL7U8fnkk',
+            spreadsheetId: '1uKqaxMd5gkghBKMmp_fbnLRFLdic_Kv-i_802f77Ih0',
+            range: 'Main!A1:G35',
+        },
+        (err, res) => {
+            if (err) return console.log('The API returned an error: ' + err);
 
-        let covidnumbers = dataNumbers.map(item => item.slice(2));
-        covidnumbers.map(item => {
-          let itemToAppend = 5 - item.length;
-          for (let i = 0; i < itemToAppend; i++) {
-            item.push("");
-          }
-        });
+            const rows = res.data.values.filter((data) => data.length !== 0);
+            if (rows.length) {
+                let headers = rows[0]
+                    .concat(rows[1])
+                    .filter(
+                        (i) =>
+                            i.length !== 0 &&
+                            i !== 'MOHFW Data' &&
+                            i !== 'State/Region'
+                    );
+                let totalOfCovid = rows[rows.length - 1].filter(
+                    (i) => i.length !== 0
+                );
+                let dataNumbers = rows.slice(2).slice(0, -1);
+                let states = dataNumbers.map(
+                    (item) => item.filter((i) => isNaN(parseInt(i)))[0]
+                );
 
-        const zip = (a1, a2) => {
-          return a1.map((_, i) => [a1[i], a2[i]]);
-        };
+                let covidnumbers = dataNumbers.map((item) => item.slice(2));
+                covidnumbers.map((item) => {
+                    let itemToAppend = 5 - item.length;
+                    for (let i = 0; i < itemToAppend; i++) {
+                        item.push('');
+                    }
+                });
 
-        let covidStates = {};
-        covidnumbers.map((item, index) => {
-          covidStates = {
-            [states[index]]: {
-              ...Object.fromEntries(zip(headers, item, states[index]))
-            },
-            ...covidStates
-          };
-        });
+                const zip = (a1, a2) => {
+                    return a1.map((_, i) => [a1[i], a2[i]]);
+                };
 
-        let totalValueData = Object.fromEntries(zip(headers, totalOfCovid));
-        covid19 = {
-          statesData: covidStates,
-          total: totalValueData
-        };
-        getOthers(token);
-      } else {
-        console.log("No data found.");
-      }
-    }
-  );
+                let covidStates = {};
+                covidnumbers.map((item, index) => {
+                    covidStates = {
+                        [states[index]]: {
+                            ...Object.fromEntries(
+                                zip(headers, item, states[index])
+                            ),
+                        },
+                        ...covidStates,
+                    };
+                });
+
+                let totalValueData = Object.fromEntries(
+                    zip(headers, totalOfCovid)
+                );
+                covid19 = {
+                    statesData: covidStates,
+                    total: totalValueData,
+                };
+                getOthers(token);
+            } else {
+                console.log('No data found.');
+            }
+        }
+    );
 };
-const getOthers = token => {
-  const sheets = google.sheets({ version: "v4", token });
-  sheets.spreadsheets.values.get(
-    {
-      auth: "AIzaSyCukHocZ6K1biwSYFPNQmOs_-OL7U8fnkk",
-      spreadsheetId: "1uKqaxMd5gkghBKMmp_fbnLRFLdic_Kv-i_802f77Ih0",
-      range: "Main!A38:G38"
-    },
-    (err, res) => {
-      let data = res.data.values[0].filter(item => item.length !== 0);
-      let values = data[0].split(": ");
-      let source = {
-        link: values[1].trim()
-      };
-      covid19 = {
-        ...covid19,
-        source
-      };
-      fs.writeFile(COVID_JSON_PATH, JSON.stringify(covid19), err => {
-        if (err) return console.error(err);
-        console.log("Response object stored to", COVID_JSON_PATH);
-      });
-    }
-  );
+const getOthers = (token) => {
+    const sheets = google.sheets({ version: 'v4', token });
+    sheets.spreadsheets.values.get(
+        {
+            auth: 'AIzaSyCukHocZ6K1biwSYFPNQmOs_-OL7U8fnkk',
+            spreadsheetId: '1uKqaxMd5gkghBKMmp_fbnLRFLdic_Kv-i_802f77Ih0',
+            range: 'Main!A38:G38',
+        },
+        (err, res) => {
+            let data = res.data.values[0].filter((item) => item.length !== 0);
+            let values = data[0].split(': ');
+            let source = {
+                link: values[1].trim(),
+            };
+            covid19 = {
+                ...covid19,
+                source,
+            };
+
+            var redisData = {};
+            redisData.items = { value: JSON.stringify(covid19) };
+            try {
+                client.hmset('items', redisData.items);
+            } catch (error) {
+                console.log('Error while storing data in redis', error);
+            }
+        }
+    );
 };
 
 module.exports = { CovidSheetData };
